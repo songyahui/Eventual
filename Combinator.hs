b3243e38c6be99d423f56e7c642c7984af9178b3
songyahuiapp@hotmail.com <songyahuiapp@hotmail.com>
2026-04-19 21:27:23 +0800

    m

--- Combinator.hs
+++ Combinator.hs
@@ -4,36 +4,36 @@ import Dict
 clean :: Double -> Double -> Double -> Double
 clean dirty accruals initial = dirty - accruals + initial
 
-comp1 :: Maybe Double -> Maybe Double -> Maybe Double -> Maybe Double
-comp1 dirty accruals initial =
-    pure clean 
-        <*> dirty <*> accruals <*> initial
-
-comp2 :: Maybe Double -> Maybe Double -> Maybe Double -> Maybe Double
-comp2 dirty accruals initial =
-    let accruals' = maybe 0 id accruals
-        initial' = maybe 0 id initial in
-    pure clean 
-        <*> dirty <*> pure accruals' <*> pure initial'
-
-comp3 ::
+-- old code
+comp1 ::
     String
     -> Dict String (Sum Double)
     -> Dict String (Sum Double)
     -> Dict String (Sum Double)
     -> Maybe Double
-comp3 key dirty accruals initial =
+comp1 key dirty accruals initial =
     let cleans = dirty <> Dict.map negate accruals <> initial
     in fmap getSum (Dict.lookup key cleans)
 
+-- simplified new code, but it doesn't handle missing keys as well as comp1
+comp2 :: Maybe Double -> Maybe Double -> Maybe Double -> Maybe Double
+comp2 dirty accruals initial =
+    pure clean 
+        <*> dirty <*> accruals <*> initial
+
+-- patched code that handles missing keys by treating them as zero
+comp3 :: Maybe Double -> Maybe Double -> Maybe Double -> Maybe Double
+comp3 dirty accruals initial =
+    let accruals' = maybe 0 id accruals
+        initial' = maybe 0 id initial in
+    pure clean 
+        <*> dirty <*> pure accruals' <*> pure initial'
+
 main :: IO ()
 main = do
-    let dirty = Just 100.0
-        accruals = Just 20.0
-        initial = Nothing
-    print $ comp1 dirty accruals initial
-    print $ comp2 dirty accruals initial
     let dirtyDict = Dict.fromList [("account1", Sum 100.0)]
         accrualsDict = Dict.fromList [("account1", Sum 20.0)]
         initialDict = Dict.empty
-    print $ comp3 "account1" dirtyDict accrualsDict initialDict
+    print $ "COMP1: " ++ show (comp1 "account1" dirtyDict accrualsDict initialDict)
+    print $ "COMP2: " ++ show (comp2 (Just 100.0) (Just 20.0) Nothing)
+    print $ "COMP3: " ++ show (comp3 (Just 100.0) (Just 20.0) Nothing)
